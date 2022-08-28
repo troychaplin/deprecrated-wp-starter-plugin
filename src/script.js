3b610e87b3de4a28a2acc3c2acd37afa3ed5bb57
Troy Chaplin <troychaplin@Troys-MacBook-Pro.local>
2022-08-27 21:26:20 -0400

    Cleaned up script.js

--- src/script.js
+++ src/script.js
@@ -1,21 +1,4 @@
-//
-// ------
-// Dump block styles to the console
-// https://soderlind.no/hide-block-styles-in-gutenberg/
-
 wp.domReady(() => {
-	wp.blocks.getBlockTypes().forEach((block) => {
-		if (_.isArray(block['styles'])) {
-			console.log(block.name, _.pluck(block['styles'], 'name'));
-		}
-	});
-});
-
-//
-// ------
-// Remove core blocks
-
-wp.domReady(function () {
 	const allowedCoreBlocks = [
 		'core/embed',
 		'core/gallery',
@@ -26,55 +9,68 @@ wp.domReady(function () {
 		'core/quote',
 		'core/table'
 	];
-
+	
 	const allowedCustomBlocks = [
 		'starter-block/feature-card',
 		'starter-block/hero-image'
 	];
-
+	
 	const allowedAdminBlocks = [
 		'core/html',
 		'core/shortcode'
 	];
-
+	
 	const allowedPluginBlocks = [
 		// 'gravityforms/form'
 	];
-
+	
 	const allowedBlocks = allowedCoreBlocks.concat(allowedCustomBlocks, allowedAdminBlocks, allowedPluginBlocks);
 
-	wp.blocks.getBlockTypes().forEach(function (coreBlocks) {
-		if (allowedBlocks.indexOf(coreBlocks.name) === -1) {
-			wp.blocks.unregisterBlockType(coreBlocks.name);
-		}
-	});
-});
-
-//
-// ------
-// Control what embeds are available
-// @see https://wordpress.stackexchange.com/questions/393243/limit-gutenberg-blocks-available-to-users-to-choose-from
-
-wp.domReady(function () {
-	var allowedEmbedBlocks = [
+	const allowedEmbedBlocks = [
 		'soundcloud',
 		'twitter',
 		'youtube',
 		'vimeo'
 	];
+	
+	// Set allowed blocks
+	wp.blocks.getBlockTypes().forEach(function (coreBlocks) {
+		if (allowedBlocks.indexOf(coreBlocks.name) === -1) {
+			wp.blocks.unregisterBlockType(coreBlocks.name);
+		}
+	});
 
+	// Set allowed embed blocks - https://wordpress.stackexchange.com/questions/393243/limit-gutenberg-blocks-available-to-users-to-choose-from
 	wp.blocks.getBlockType('core/embed').variations.forEach(function (blockVariation) {
 		if (allowedEmbedBlocks.indexOf(blockVariation.name) === -1) {
 			wp.blocks.unregisterBlockVariation('core/embed', blockVariation.name);
 		}
 	});
+
+	// Remove block styles
+	wp.blocks.unregisterBlockStyle('core/image', 'default');
+	wp.blocks.unregisterBlockStyle('core/image', 'rounded');
+	wp.blocks.unregisterBlockStyle('core/quote', 'default');
+	wp.blocks.unregisterBlockStyle('core/quote', 'plain');
+	wp.blocks.unregisterBlockStyle('core/table', 'regular');
+	wp.blocks.unregisterBlockStyle('core/table', 'stripes');
+	
+	// Dump block styles to the console - https://soderlind.no/hide-block-styles-in-gutenberg/
+	wp.blocks.getBlockTypes().forEach((block) => {
+		if (_.isArray(block['styles'])) {
+			console.log(block.name, _.pluck(block['styles'], 'name'));
+		}
+	});
 });
 
 //
-// ------
+// Remove other editor panels
+
+wp.data.dispatch('core/edit-post').removeEditorPanel('discussion-panel');
+
+//
 // Remove core block elements via the block support API
 // @see https://css-tricks.com/a-crash-course-in-wordpress-block-filters/
-// ------
 // @see https://stackoverflow.com/questions/71637137/remove-property-from-wordpress-core-gutenberg-block
 // @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-supports/#typography
 // @see https://javascriptforwp.com/extending-wordpress-blocks/
@@ -115,22 +111,3 @@ addFilter(
 	'cuBlockModifications',
 	extendCoreBlocks,
 );
-
-//
-// ------
-// Remove block options
-
-wp.domReady(function () {
-	wp.blocks.unregisterBlockStyle('core/image', 'default');
-	wp.blocks.unregisterBlockStyle('core/image', 'rounded');
-	wp.blocks.unregisterBlockStyle('core/quote', 'default');
-	wp.blocks.unregisterBlockStyle('core/quote', 'plain');
-	wp.blocks.unregisterBlockStyle('core/table', 'regular');
-	wp.blocks.unregisterBlockStyle('core/table', 'stripes');
-});
-
-//
-// ------
-// Remove other editor panels
-
-wp.data.dispatch('core/edit-post').removeEditorPanel('discussion-panel');
