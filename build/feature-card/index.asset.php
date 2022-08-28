f512d1ce1b159cc36019a93493870502c3c8805c
Troy Chaplin <troychaplin@Troys-MacBook-Pro.local>
2022-08-27 21:13:07 -0400

    Fixed block editor styles

--- build/feature-card/index.asset.php
+++ build/feature-card/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '087f91a2ed6ebc2faabb');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'c2daece3c84e60548121');
--- build/hero-image/index.asset.php
+++ build/hero-image/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '00849f859d1e117e12d1');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '0364195acfbae808376d');
